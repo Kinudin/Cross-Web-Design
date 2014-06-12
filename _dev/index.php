00fde0625d1c112ced5207e3115521333b3a87b0
Eric Cross <crosse88@gmail.com>
2014-06-12 09:04:49 -0700

    Initial Commit
    Hooray! The first commit for my portfolio site

---
+++ _dev/index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Eric Cross | Portfolio</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+  <!-- NAVIGATION -->
+  <div class="row">
+    <div class="small-4 large-2 columns">
+      <img src="img/me.png" height="119" width="134" alt="" id="mePic" class="medium-pull-2 hide-for-small-down">
+    </div>
+    <div class="small-8 medium-push-1 large-5 columns nav hide-for-small-only">
+      <ul>
+        <li>
+          <a data-scroll-nav="0">About</a>
+          <a data-scroll-nav="1">Work</a>
+          <a data-scroll-nav="2">Contact</a>
+          <a href="/blog">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- INTRO SLIDE -->
+  <div class="row">
+    <div id="personalInfo" class="small-12 large-12 columns">
+      <div class="large-12 small-12 small-centered name">
+        <span id="name1">Eric Cross</span>
+        <br>
+        <span id="tagLine">Web Designer / Developer</span>
+        <br>
+        <a href="#" class="button round">Contact me</a>
+      </div>
+    </div>
+  </div>
+  <!-- About me slide -->
+  <div class="row" data-scroll-index="0">
+  <div id="aboutMe">
+    <div class="small-4 large-6 columns" id="aboutMeImg">
+      <img src="img/aboutMeSide.png" height="450" width="500">
+    </div>
+    <section>
+      <header><h1>About me</h1></header>
+      <article>
+        I am a 26 year old Web Designer & Developer student based out of San Diego, California. While on this journey I have found a passion with clean user interfaces and making the web a better place through well paired typography and color schemes.
+      </article>
+    </section>
+    
+    <div class="small-12 large-6 columns small-block-grid-4" id="aboutMe">
+      <ul>
+        <li>
+          <img src="img/aboutMe/UX.png" height="80" width="80" alt="">
+          <header>
+            User Experience / User Interface (UX/UI)
+          </header>
+          <section>The web deserves to be a beautiful place. I’m here to make sure that happens.</section>
+        </li>
+        <li>
+            <img src="img/aboutMe/webdev.png" height="80" width="80" alt="">
+            <header>
+              Web Development
+            </header>
+            <section>
+              The engine that runs the car needs to be clean and only run on the best materials.
+            </section>
+        </li>
+        <li>
+            <img src="img/aboutMe/dd.png" height="80" width="80" alt="">
+            <header>
+              Data Design
+            </header>
+            <section>
+              Organized data is accessible data. I strive to eliminate redundancy and streamline processes.
+            </section>
+        </li>
+        <li>
+          <img src="img/aboutMe/bicycle.png" height="80" width="80" alt="">
+            <header>
+              Bicycle Rider
+            </header>
+            <section>
+              My inspiration comes from time outside.
+            </section>
+          </li>
+      </ul>
+      
+    </div>
+  </div>
+  </div>
+  <!-- Work Slide -->
+    <div id="work" data-scroll-index="1">
+      <div class="small-12 large-12 small-text-center large-text-center">
+          <h1>WORK</h1>
+          <!-- Thumbnailing project -->
+          <div class="small-12 columns medium-6 large-4 small-text-left large-text-left">
+            <a href="http://9bit.net/advwebfinal/index.php" target="_blank">
+              <header class="text-center">Thumbnailing!</header>
+              <article>
+                <img src="img/work/thumbnailing.png" height="202" width="300" alt=""></a>
+                <section>
+                This was a school project. It used PHP, JavaScript, the Skeleton framework, and lightbox to show off the images.
+                </section>
+              </article>
+          </div>
+          <!-- Art of Beer project -->
+          <div class="small-12 columns medium-6 large-4 small-text-left large-text-left">
+            <a href="http://9bit.net/aobRedesign/" target="_blank">
+              <header class="text-center">The Art of Beer</header>
+              <article>
+                <img src="img/work/artOfBeer.png" height="202" width="300" alt=""></a>
+                <section>
+                The Art of Beer was a personal project with the challenge of creating an informative single page site. Javascript, jQuery, HTML5 & CSS were used.
+                </section>
+              </article>
+          </div>
+          <!-- 9bit.net Branding project -->
+          <div class="small-12 columns medium-12 large-4 small-text-left large-text-left" style="padding-top: 10px;">
+              <header class="text-center small-pull-0 medium-pull-0 large-pull-2">9bit.net Branding Project</header>
+              <article>
+                <!-- INSERT UL FOR CLEARING (LIGHTBOX) HERE -->
+                <ul class="clearing-thumbs clearing-feature small-push-1 medium-push-4 large-push-0" data-clearing>
+                  <li><a href="img/work/9bit/businesscard.png" height="1050" width="600"><img src="img/work/9bit/businesscard.png"></a></li>
+                  <li><a href="img/work/9bit/letterhead.png" height="3300" width="2550"><img src="img/work/9bit/letterhead.png"></a></li>
+                  <li class="clearing-featured-img"><a href="img/work/9bit/logo.png" height="202" width="303"><img src="img/work/9bit/9bth.png"></a></li>
+                  <li><a href="img/work/9bit/color pallette.jpg"><img src="img/work/9bit/color pallette.jpg" height="164" width="341" alt=""></a></li>
+                  <li><a href="img/work/9bit/envelopeclosed.jpg"><img src="img/work/9bit/envelopeclosed.jpg" alt=""></a></li>
+                  <li><a href="img/work/9bit/envelopefront.png" height="1313" width="2925"><img src="img/work/9bit/envelopefront.png" alt=""></a></li>
+                  <li><a href="img/work/9bit/envelopeopen.jpg"><img src="img/work/9bit/envelopeopen.jpg" height="1725" width="2813" alt=""></a></li>
+                  <li><a href="img/work/9bit/flyer.png" height="1200" width="1500"><img src="img/work/9bit/flyer.png" alt=""></a></li>
+                </ul>
+                <section>
+                This was a graphic design project intended to create an entire stationary package for a client. I chose to use 9bit.net, my original brand name. Photoshop and Illustrator were used.
+                </section>
+              </article>
+            </a>
+          </div>
+
+      </div>
+    </div>
+
+
+    <!-- Contact form -->
+
+
+    <div id="contact" data-scroll-index="2">
+      <div class="row">
+        <div class="small-12 medium-6 large-3 columns">
+          <h2 class="small-only-text-center">Let's chat</h2>
+          <section>
+            I would love to talk with you about your upcoming project!
+          </section>
+      </div>
+        <div class="small-12 medium-6 large-9 columns">
+          <div class="row">
+          <form id="ajax-contact" method="post" action="php/mailer.php" data-abide>
+              <!-- Name -->
+              <div class="row">
+            <div class="small-6 large-2 columns">
+              <label for="name" class="inline">Your Name</label>
+            </div>
+            <div class="small-6 large-7 large-pull-3 small-pull-3 columns">
+              <input type="text" id="name" name="name" placeholder="Your Name">
+            </div>
+            </label>
+          </div>
+            
+              <!-- Email Address -->
+          <div class="row">
+              <div class="small-6 large-2 columns">
+                <label for="email" class="inline">Email Address</label>
+              </div>
+              <div class="small-6 large-7 large-pull-3 small-pull-3 columns">
+                <input type="email" id="email" name="email" placeholder="john@gmail.com" required>
+              </div>
+                </label>
+            </div> <!-- Closes the row for this contact form -->
+              <!-- Message Address -->
+            <div class="row">
+              <div class="small-6 large-2 columns">
+                <label for="message" class="inline">Your Message</label>
+              </div>
+              <div class="small-6 large-7 large-pull-3 small-pull-3 columns">
+                <input type="textarea" class="large-12" rows="20" id="message" name="message" placeholder="I really want you to make my next portfolio page!" required>
+              </div>
+                </label>
+            </div> <!-- Closes the row for this contact form -->
+            <button type="submit" class="small-push-6 large-push-7 move-right">Send away</button>
+          </form> <!-- Closes the form, yay-->
+          <div class="small-12 large-3" id="form-messages"></div>
+</div>        
+        </div>
+      </div>
+    </div>
+
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/app.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script src="js/scrollIt.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+  <script>
+  $(function(){
+    $.scrollIt();
+  });
+  </script>
+</body>
+</html>
